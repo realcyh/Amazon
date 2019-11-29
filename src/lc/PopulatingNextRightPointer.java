9ec65e7106c6d1b40c572318b49b596e4676de1b
YH CHEN <realcyh@outlook.com>
2019-11-29 00:29:47 -0500

    lc116 Populating Next Right Pointers in Each Node

---
+++ src/lc/PopulatingNextRightPointer.java
@@ -0,0 +1,66 @@
+package lc;
+
+public class PopulatingNextRightPointer {
+    public class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    // recursive
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.left != null) {
+            root.left.next = root.right;
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+
+    //iterative
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node start = root;
+        while (start != null) {
+            Node cur = start;
+            while (cur != null) {
+                if (cur.left != null) {
+                    cur.left.next = cur.right;
+                }
+                if (cur.right != null && cur.next != null) {
+                    cur.right.next = cur.next.left;
+                }
+                cur = cur.next;
+            }
+            start = start.left;
+        }
+
+        return root;
+
+    }
+
+}
