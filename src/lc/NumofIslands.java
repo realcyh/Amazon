03b84e3add3cf5fffd5cf05047a6c76d47ac643f
YH CHEN <realcyh@outlook.com>
2019-12-13 22:01:30 -0500

    lc200 number of islands bfs/dfs

--- src/lc/NumofIslands.java
+++ src/lc/NumofIslands.java
@@ -1,6 +1,10 @@
 package lc;
 
+import java.util.*;
+
 public class NumofIslands {
+
+    // dfs
     private int m, n;
     public int numIslands(char[][] grid) {
         int cnt = 0;
@@ -27,11 +31,44 @@ public class NumofIslands {
         dfsMark(grid, i, j+1);
     }
 
+
+    // bfs
+    public int numberOfIslansBFS(char[][] grid) {
+        if (grid.length == 0) return 0;
+        int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        Queue<int[]> queue = new LinkedList<>();
+        int count = 0;
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    queue.offer(new int[]{i,j});
+                    while (!queue.isEmpty()) {
+                        int curr[] = queue.poll();
+                        for (int[] dir: dirs) {
+                            int x = curr[0] + dir[0];
+                            int y = curr[1] + dir[1];
+                            if (x<0 || y< 0 || x>=grid.length || y>=grid[0].length || grid[x][y] == '0') continue;
+                            grid[x][y] = '0';
+                            queue.offer(new int[] {x, y});
+                        }
+                    }
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+
     public static void main(String[] args) {
         NumofIslands ni = new NumofIslands();
         char[][] grid = {{'1','1','0'}, {'0','0','0'}, {'1','1','1'}};
         int res = ni.numIslands(grid);
         System.out.println(res);
+        grid = new char[][]{{'1','1','0'}, {'0','0','0'}, {'1','1','1'}};
+        int res2 = ni.numberOfIslansBFS(grid);
+        System.out.println(res2);
 
     }
 }
