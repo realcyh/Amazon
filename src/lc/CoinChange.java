7c9870bd8ef5f5e24ad73aadbc11e04acee396a0
YH CHEN <realcyh@outlook.com>
2019-12-18 16:32:45 -0500

    lc322 Coin Change

--- src/lc/CoinChange.java
+++ src/lc/CoinChange.java
@@ -3,6 +3,7 @@ package lc;
 import java.util.*;
 
 public class CoinChange {
+    // iterative bottom up
     public int coinChange(int[] coins, int amount) {
         //if (coins.length == 0 || amount<=0) return -1;
         int[] dp = new int[amount+1];
@@ -18,5 +19,28 @@ public class CoinChange {
         }
         if (dp[amount] > amount) return -1;
         else return dp[amount];
+    }
+
+    // recursive top down
+    public int coinChange2(int[] coins, int amount) {
+        if (amount<1) return 0;
+        int[] count = new int[amount+1];
+        count[0] = 0;
+        return helper(coins, amount, count);
+    }
+
+    public int helper(int[] coins, int remain, int[] count) {
+        if (remain<0) return -1;
+        if (remain == 0) return 0;
+        if (count[remain] != 0) return count[remain];
+        int min = Integer.MAX_VALUE;
+        for (int coin: coins) {
+            int res = helper(coins, remain-coin, count);
+            if (res>=0 && res<min) {
+                min = 1+res;
+            }
+        }
+        count[remain] = (min== Integer.MAX_VALUE) ? -1: min;
+        return count[remain];
     }
 }
