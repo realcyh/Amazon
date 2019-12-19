c0566fc407db3ddcbe351bd0b8142d99252e31ce
YH CHEN <realcyh@outlook.com>
2019-12-18 23:27:17 -0500

    lc71 Simplify Path

---
+++ src/lc/SimplifyPath.java
@@ -0,0 +1,23 @@
+package lc;
+
+import java.util.*;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        Set<String> set = new HashSet<>();
+        set.add("..");
+        set.add(".");
+        set.add("");
+        for (String dir: path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
+            else if (!set.contains(dir)) stack.push(dir);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String dir: stack) {
+            sb.append("/"+dir);
+        }
+        if (sb.length() == 0) return "/";
+        return sb.toString();
+    }
+}
